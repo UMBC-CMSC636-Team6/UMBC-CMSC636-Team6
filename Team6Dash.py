08bece2eb5ef662b00eb161953be87d07f25585a
cdevoe12 <60455290+cdevoe12@users.noreply.github.com>
2025-04-16 10:43:57 -0400

    Working to Get Render Working

--- Team6Dash.py
+++ Team6Dash.py
@@ -201,6 +201,6 @@ def main():
     if __name__ == "__main__":
         port = int(os.environ.get("PORT", 8050))
         # app.run(debug=True)
-        app.server.run(debug=True, port = port)
+        app.server.run(debug=True, host='0.0.0.0', port = port)
 
 main()
